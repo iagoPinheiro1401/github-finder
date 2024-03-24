7713e1fdc58ab34aab19f2c468dddac42d4e0a68
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2024-03-24 11:04:29 -0300

    react-dom configurator for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Outlet } from "react-router-dom"
 
 function App() {
   return (
     <div>
       <h1 className="font-bold text-[2rem]">Inscreva-se</h1>
+      <Outlet/>
     </div>
   )
 }
