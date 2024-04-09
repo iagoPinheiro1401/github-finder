efead765a6c0e3abfdf3a44ec621531d2b811aae
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2024-04-09 09:14:59 -0300

    created component for user not found

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,15 +2,26 @@ import { UserProps } from "../types/user"
 
 import Search from "../components/search/Search"
 import User from "../components/user/User"
+import Error from "../components/error/Error"
 
 import { useState } from "react"
 
 export default function Home() {
     const [user, setUser] = useState<UserProps | null>(null)
+    const [error, setError] = useState(false)
 
     const loadUser = async (userName: string) => {
+        setError(false)
+        setUser(null)
+
         const response = await fetch(`https://api.github.com/users/${userName}`)
         const data = await response.json()
+
+        if(response.status == 404) {
+            setError(true)
+            return
+        }
+
         const { avatar_url, login, location, followers, following } = data
         const userData: UserProps = {
             avatar_url,
@@ -26,6 +37,7 @@ export default function Home() {
         <div>
             <Search loadUser={loadUser}/>
             {user && <User {...user}/>}
+            {error && <Error/>  }
         </div>
     )
 }
