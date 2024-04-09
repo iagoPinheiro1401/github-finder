1f5383434eba565f0e6a543f90e53035fad70bd6
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2024-04-09 10:04:56 -0300

    data styling

--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -6,26 +6,30 @@ import { Link } from "react-router-dom"
 
 export default function User({ login, avatar_url, followers, following, location }: UserProps) {
     return(
-        <div>
-            <img src={avatar_url} alt={login}/>
+        <div className="bg-blue-950 p-8 rounded-2xl flex flex-col items-center justify-center gap-4">
+            <img 
+                src={avatar_url} 
+                alt={login}
+                className="w-36 h-36 border-4 border-purple-700 rounded-full"
+            />
             <h2>{login}</h2>
             {location && (
-                <p>
-                    <MdLocationPin />
-                    <span>{location}</span>
+                <p className="flex justify-center items-center gap-1">
+                    <MdLocationPin className="fill-green-300 size-6"/>
+                    <span className="text-blue-300 font-bold">{location}</span>
                 </p>
             )}
-            <div>
-                <div>
+            <div className="flex ">
+                <div className="flex flex-col px-4 border-r border-e-blue-300 gap-2">
                     <p>Seguidores:</p>
-                    <p>{followers}</p>
+                    <p className="bg-green-300 px-2 py-1 rounded">{followers}</p>
                 </div>
-                <div>
+                <div className="flex flex-col px-4 gap-2">
                     <p>Seguindo:</p>
-                    <p>{following}</p>
+                    <p className="bg-green-300 px-2 py-1 rounded">{following}</p>
                 </div>
             </div>
-            <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
+            <Link className="p-4 bg-purple-700 rounded-md text-center opacity-80 hover:opacity-100 transition-all duration-300" to={`/repos/${login}`}>Ver melhores projetos</Link>
         </div>
     )
 }
